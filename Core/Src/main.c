9ad43202b4a25c7741fc18669431b5f053565d2e
Mariusz Jacek <marios.yaceg@gmail.com>
2025-03-27 22:58:03 +0100

    Init

---
+++ Core/Src/main.c
@@ -0,0 +1,622 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "app_threadx.h"
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <stdio.h>
+#include <stdarg.h>
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+#define LTR390_UVSMode 0x02
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+I2C_HandleTypeDef hi2c1;
+
+RTC_HandleTypeDef hrtc;
+
+UART_HandleTypeDef huart1;
+
+/* USER CODE BEGIN PV */
+
+char RTCBuffer [100];
+extern I2C_HandleTypeDef hi2c1;
+extern UART_HandleTypeDef huart1;
+
+float lux_index_global = 0.0;
+
+extern FX_FILE file;
+extern FX_MEDIA sd_disk;
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_ICACHE_Init(void);
+static void MX_I2C1_Init(void);
+static void MX_USART1_UART_Init(void);
+static void MX_RTC_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+void UART_Printf(const char *fmt, ...){
+	char buffer[100];
+	va_list args;
+	va_start(args, fmt);
+	vsnprintf(buffer, sizeof(buffer), fmt, args);
+	va_end(args);
+	HAL_UART_Transmit(&huart1, (uint8_t*)buffer, strlen(buffer), HAL_MAX_DELAY);
+}
+
+void write_lux_to_file(const char *data){
+
+	UINT status;
+
+	fx_file_seek(&file, 0, FX_SEEK_END);
+
+	status = fx_file_write(&file, data, strlen(data));
+
+	if(status != FX_SUCCESS){
+		printf("Failed to save data %u\n", status);
+	}
+
+	fx_file_flush(&file);
+}
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_ICACHE_Init();
+  MX_I2C1_Init();
+  MX_USART1_UART_Init();
+  MX_RTC_Init();
+  /* USER CODE BEGIN 2 */
+
+  //Timer initialization
+  RTC_TimeTypeDef sTime;
+  RTC_DateTypeDef sDate;
+
+
+  //UV measurement
+  HAL_Delay(1000);
+  uint8_t buf_from[1] = {0};
+
+  UART_Printf("Put device in ALS mode \r\n");
+  LTR390_WriteRegister(LTR390UV_HOLDINGREG_MAIN_CTRL, LTR390_ALSMode);
+
+
+  uint8_t buf[1];
+  LTR390_ReadRegister(LTR390UV_HOLDINGREG_MAIN_CTRL, buf, 1);
+
+  HAL_Delay(10);
+
+  // read from LTR390UV_HOLDINGREG_MAIN_CTRL
+  LTR390_ReadRegister(LTR390UV_HOLDINGREG_MAIN_CTRL, buf_from, 1);
+  UART_Printf("MAIN_CTRL: 0x%02X\r\n", buf_from[0]);
+
+  HAL_Delay(1000);
+
+  // Set measurement parameters
+  UART_Printf("\r\nSet measurement parameters\r\n");
+  LTR390_WriteRegister(LTR390UV_HOLDINGREG_ALS_UVS_MEAS_RATE, LTR390_RES18_BIT + LTR390_MEASRATE_100MS);
+
+  HAL_Delay(10);
+
+  // read from LTR390UV_HOLDINGREG_MAIN_CTRL
+  LTR390_ReadRegister(LTR390UV_HOLDINGREG_ALS_UVS_MEAS_RATE, buf_from, 1);
+  UART_Printf("MEAS_RATE: 0x%02X\r\n", buf_from[0]);
+
+  // Set gain
+  UART_Printf("\r\nSet gain\r\n");
+  LTR390_WriteRegister(LTR390UV_HOLDINGREG_ALS_UVS_GAIN, LTR390_GAIN3);
+
+  HAL_Delay(10);
+
+  // read from LTR390UV_HOLDINGREG_MAIN_CTRL
+  LTR390_ReadRegister(LTR390UV_HOLDINGREG_ALS_UVS_GAIN, buf_from, 1);
+  UART_Printf("GAIN: 0x%02X\r\n", buf_from[0]);
+
+  HAL_Delay(100);
+
+  MX_FileX_Init();
+  /* USER CODE END 2 */
+
+  MX_ThreadX_Init();
+
+  /* We should never get here as control is now taken by the scheduler */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+	  	  HAL_Delay(500);
+	 	  //read ALS measurement after 1s
+	 	  uint8_t a_gain[5] = {1,3,6,9,18};
+	 	  double a_int[6] = {4.,2.,1.,0.5,0.25,0.03125}; //lux
+	 	  uint8_t gain = LTR390_GAIN3; //lux
+	 	  uint8_t resolution = (LTR390_RES18_BIT & 0xf0) >> 4; //lux
+	 	  uint8_t buffer[4] = {0};
+	 	  uint32_t originalData = 0;
+	 	  float lux = 0; //lux
+//	 	  float uv_index = 0;
+
+
+	 	  UART_Printf("Reading AL data...\r\n");
+	 	  LTR390_ReadRegister(LTR390UV_INPUTREG_ALS_DATA_LOW, buffer, 4);
+	 	  UART_Printf("Buffer: %02X %02X %02X %02X\r\n", buffer[0], buffer[1], buffer[2], buffer[3]);
+
+
+	 	  UART_Printf("MAIN_CTRL register: 0x%02X\r\n", buf[0]);
+	 	  if (buf[0] != LTR390_UVSMode) {
+	 	      UART_Printf("UV mode off!\r\n");
+	 	  }else{
+	 		  UART_Printf(" UV mode on !\r\n");
+	 	  }
+
+	 	  uint8_t size = 4;
+	 	  for(uint8_t i = 0; i < size;){
+	 		uint8_t temp = buffer[i];
+	 		buffer[i] = buffer[i+1];
+	 		buffer[i+1] = temp;
+	 		i+=2;
+	 	  }
+
+	 	  originalData = (uint32_t)buffer[2] << 24 | (uint32_t)buffer[3] << 16 | (uint16_t)buffer[0] << 8 | buffer[1]; //lux
+	 	  lux = (0.6 * originalData) / (a_gain[gain] * a_int[resolution]); //lux
+
+//	 	  originalData = (uint32_t)buffer[3] << 24 |
+//	 	                 (uint32_t)buffer[2] << 16 |
+//	 	                 (uint32_t)buffer[1] << 8 |
+//	 	                 (uint32_t)buffer[0];
+//	 	  uv_index = (float)originalData / (a_gain[gain] * a_int[resolution]);
+
+	 	  UART_Printf("Original data: %lu\r\n", originalData);
+	 	  UART_Printf("LUX dataa: %f\r\n", lux);
+//	 	  UART_Printf("UV Index: %f\r\n", uv_index);
+
+	 	  //Timer
+	 	  HAL_RTC_GetTime(&hrtc, &sTime, RTC_FORMAT_BIN);
+
+	 	  HAL_RTC_GetData(&hrtc, &sDate, RTC_FORMAT_BIN);
+
+	 	  snprintf(RTCBuffer, sizeof(RTCBuffer), "Time:%02d:%02d:%02d\r\n",
+	 			  sTime.Hours,
+				  sTime.Minutes,
+				  sTime.Seconds
+				  );
+
+		   HAL_UART_Transmit(&huart1, (uint8_t*)RTCBuffer, strlen(RTCBuffer), HAL_MAX_DELAY);
+		   HAL_Delay(1000);
+
+	 	  // Formatowanie do zapisywania na SD
+	 	  char buffer[50];
+	 	  snprintf(buffer, sizeof(buffer), "LUX data: %f\r\n", lux);
+
+
+	 	  write_lux_to_file(buffer);
+	 	  HAL_Delay(1000);
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE4) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_LSI|RCC_OSCILLATORTYPE_MSI;
+  RCC_OscInitStruct.LSIState = RCC_LSI_ON;
+  RCC_OscInitStruct.MSIState = RCC_MSI_ON;
+  RCC_OscInitStruct.MSICalibrationValue = RCC_MSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_4;
+  RCC_OscInitStruct.LSIDiv = RCC_LSI_DIV1;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2
+                              |RCC_CLOCKTYPE_PCLK3;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_MSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB3CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief I2C1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C1_Init(void)
+{
+
+  /* USER CODE BEGIN I2C1_Init 0 */
+
+  /* USER CODE END I2C1_Init 0 */
+
+  /* USER CODE BEGIN I2C1_Init 1 */
+
+  /* USER CODE END I2C1_Init 1 */
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.Timing = 0x00000E14;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Analogue filter
+  */
+  if (HAL_I2CEx_ConfigAnalogFilter(&hi2c1, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Digital filter
+  */
+  if (HAL_I2CEx_ConfigDigitalFilter(&hi2c1, 0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C1_Init 2 */
+
+  /* USER CODE END I2C1_Init 2 */
+
+}
+
+/**
+  * @brief ICACHE Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_ICACHE_Init(void)
+{
+
+  /* USER CODE BEGIN ICACHE_Init 0 */
+
+  /* USER CODE END ICACHE_Init 0 */
+
+  /* USER CODE BEGIN ICACHE_Init 1 */
+
+  /* USER CODE END ICACHE_Init 1 */
+
+  /** Enable instruction cache in 1-way (direct mapped cache)
+  */
+  if (HAL_ICACHE_ConfigAssociativityMode(ICACHE_1WAY) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_ICACHE_Enable() != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN ICACHE_Init 2 */
+
+  /* USER CODE END ICACHE_Init 2 */
+
+}
+
+/**
+  * @brief RTC Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_RTC_Init(void)
+{
+
+  /* USER CODE BEGIN RTC_Init 0 */
+
+  /* USER CODE END RTC_Init 0 */
+
+  RTC_PrivilegeStateTypeDef privilegeState = {0};
+  RTC_TimeTypeDef sTime = {0};
+  RTC_DateTypeDef sDate = {0};
+
+  /* USER CODE BEGIN RTC_Init 1 */
+
+  /* USER CODE END RTC_Init 1 */
+
+  /** Initialize RTC Only
+  */
+  hrtc.Instance = RTC;
+  hrtc.Init.HourFormat = RTC_HOURFORMAT_24;
+  hrtc.Init.AsynchPrediv = 127;
+  hrtc.Init.SynchPrediv = 255;
+  hrtc.Init.OutPut = RTC_OUTPUT_DISABLE;
+  hrtc.Init.OutPutRemap = RTC_OUTPUT_REMAP_NONE;
+  hrtc.Init.OutPutPolarity = RTC_OUTPUT_POLARITY_HIGH;
+  hrtc.Init.OutPutType = RTC_OUTPUT_TYPE_OPENDRAIN;
+  hrtc.Init.OutPutPullUp = RTC_OUTPUT_PULLUP_NONE;
+  hrtc.Init.BinMode = RTC_BINARY_NONE;
+  if (HAL_RTC_Init(&hrtc) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  privilegeState.rtcPrivilegeFull = RTC_PRIVILEGE_FULL_NO;
+  privilegeState.backupRegisterPrivZone = RTC_PRIVILEGE_BKUP_ZONE_NONE;
+  privilegeState.backupRegisterStartZone2 = RTC_BKP_DR0;
+  privilegeState.backupRegisterStartZone3 = RTC_BKP_DR0;
+  if (HAL_RTCEx_PrivilegeModeSet(&hrtc, &privilegeState) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /* USER CODE BEGIN Check_RTC_BKUP */
+
+  /* USER CODE END Check_RTC_BKUP */
+
+  /** Initialize RTC and set the Time and Date
+  */
+  sTime.Hours = 0;
+  sTime.Minutes = 0;
+  sTime.Seconds = 0;
+  sTime.DayLightSaving = RTC_DAYLIGHTSAVING_NONE;
+  sTime.StoreOperation = RTC_STOREOPERATION_RESET;
+  if (HAL_RTC_SetTime(&hrtc, &sTime, RTC_FORMAT_BIN) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sDate.WeekDay = RTC_WEEKDAY_MONDAY;
+  sDate.Month = RTC_MONTH_JANUARY;
+  sDate.Date = 1;
+  sDate.Year = 0;
+
+  if (HAL_RTC_SetDate(&hrtc, &sDate, RTC_FORMAT_BIN) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN RTC_Init 2 */
+
+  /* USER CODE END RTC_Init 2 */
+
+}
+
+/**
+  * @brief USART1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART1_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART1_Init 0 */
+
+  /* USER CODE END USART1_Init 0 */
+
+  /* USER CODE BEGIN USART1_Init 1 */
+
+  /* USER CODE END USART1_Init 1 */
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 115200;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart1.Init.ClockPrescaler = UART_PRESCALER_DIV1;
+  huart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_UARTEx_SetTxFifoThreshold(&huart1, UART_TXFIFO_THRESHOLD_1_8) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_UARTEx_SetRxFifoThreshold(&huart1, UART_RXFIFO_THRESHOLD_1_8) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_UARTEx_DisableFifoMode(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART1_Init 2 */
+
+  /* USER CODE END USART1_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+
+  /* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(SD_CS_GPIO_Port, SD_CS_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : PA2 PA3 */
+  GPIO_InitStruct.Pin = GPIO_PIN_2|GPIO_PIN_3;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.Alternate = GPIO_AF8_LPUART1;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : SD_CS_Pin */
+  GPIO_InitStruct.Pin = SD_CS_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(SD_CS_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PB13 PB14 PB15 */
+  GPIO_InitStruct.Pin = GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.Alternate = GPIO_AF5_SPI2;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+
+  /* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  Period elapsed callback in non blocking mode
+  * @note   This function is called  when TIM6 interrupt took place, inside
+  * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+  * a global variable "uwTick" used as application time base.
+  * @param  htim : TIM handle
+  * @retval None
+  */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+  /* USER CODE BEGIN Callback 0 */
+
+  /* USER CODE END Callback 0 */
+  if (htim->Instance == TIM6)
+  {
+    HAL_IncTick();
+  }
+  /* USER CODE BEGIN Callback 1 */
+
+  /* USER CODE END Callback 1 */
+}
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
---
+++ FileX/App/app_filex.c
@@ -0,0 +1,183 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    app_filex.c
+  * @author  MCD Application Team
+  * @brief   FileX applicative file
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "app_filex.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* Main thread stack size */
+#define FX_APP_THREAD_STACK_SIZE         512
+/* Main thread priority */
+#define FX_APP_THREAD_PRIO               10
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+/* Main thread global data structures.  */
+TX_THREAD       fx_app_thread;
+
+/* USER CODE BEGIN PV */
+FX_MEDIA sd_disk;
+FX_FILE file;
+CHAR file_name[] = "data.txt";
+
+UCHAR media_memory[100];
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+
+/* Main thread entry function.  */
+void fx_app_thread_entry(ULONG thread_input);
+
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/**
+  * @brief  Application FileX Initialization.
+  * @param memory_ptr: memory pointer
+  * @retval int
+*/
+UINT MX_FileX_Init(VOID *memory_ptr)
+{
+  UINT ret = FX_SUCCESS;
+  TX_BYTE_POOL *byte_pool = (TX_BYTE_POOL*)memory_ptr;
+  VOID *pointer;
+
+/* USER CODE BEGIN MX_FileX_MEM_POOL */
+
+/* USER CODE END MX_FileX_MEM_POOL */
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/*Allocate memory for the main thread's stack*/
+  ret = tx_byte_allocate(byte_pool, &pointer, FX_APP_THREAD_STACK_SIZE, TX_NO_WAIT);
+
+/* Check FX_APP_THREAD_STACK_SIZE allocation*/
+  if (ret != FX_SUCCESS)
+  {
+    return TX_POOL_ERROR;
+  }
+
+/* Create the main thread.  */
+  ret = tx_thread_create(&fx_app_thread, FX_APP_THREAD_NAME, fx_app_thread_entry, 0, pointer, FX_APP_THREAD_STACK_SIZE,
+                         FX_APP_THREAD_PRIO, FX_APP_PREEMPTION_THRESHOLD, FX_APP_THREAD_TIME_SLICE, FX_APP_THREAD_AUTO_START);
+
+/* Check main thread creation */
+  if (ret != FX_SUCCESS)
+  {
+    return TX_THREAD_ERROR;
+  }
+
+/* USER CODE BEGIN MX_FileX_Init */
+
+/* USER CODE END MX_FileX_Init */
+
+/* Initialize FileX.  */
+  fx_system_initialize();
+
+/* USER CODE BEGIN MX_FileX_Init 1*/
+
+/* USER CODE END MX_FileX_Init 1*/
+
+  return ret;
+}
+
+/**
+ * @brief  Main thread entry.
+ * @param thread_input: ULONG user argument used by the thread entry
+ * @retval none
+*/
+ void fx_app_thread_entry(ULONG thread_input)
+ {
+
+/* USER CODE BEGIN fx_app_thread_entry 0*/
+
+/* USER CODE END fx_app_thread_entry 0*/
+
+/* USER CODE BEGIN fx_app_thread_entry 1*/
+	 UINT status;
+	 ULONG actual_length = 0;
+	 CHAR write_data[] = "Hello World data: 1234\n";
+
+	 status = fx_media_open(&sd_disk, "SD_CARD", fx_stm32_sd_driver, NULL, media_memory, sizeof(media_memory));
+
+	 if(status != FX_SUCCESS){
+		 printf("Program cannot open SD card!: %u\n", status);
+		 return;
+	 }
+
+	 status = fx_file_create(&sd_disk, file_name);
+
+	 if(status != FX_SUCCESS && status != FX_ALREADY_CREATED){
+		 printf("program cannot create file: %u\n",status);
+		 return;
+	 }
+
+	 status = fx_file_open(&sd_disk, &file, file_name, FX_OPEN_FOR_WRITE);
+
+	 if(status != FX_SUCCESS){
+		 printf("program cannot open file: %u\n",status);
+		 return;
+	 }
+
+	 fx_file_seek(&file, 0); //pointer to the EOF
+
+	 while(1){
+
+		 status = fx_file_write(&file, write_data, sizeof(write_data) - 1);
+
+		 if(status != FX_SUCCESS){
+			 printf("Program cannot save data: %u\n", status);
+			 break;
+		 }
+
+		 printf("Data saved! %s\n", write_data);
+		 fx_file_flush(&file);
+		 tx_thread_sleep(100);
+	 }
+
+	 fx_file_close(&file);
+	 fx_media_close(&sd_disk);
+/* USER CODE END fx_app_thread_entry 1*/
+  }
+
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
